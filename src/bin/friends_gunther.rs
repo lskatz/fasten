b60e4b2d5feefe11049ca36a061fe12e4e8df82e
Lee Katz <lskatz@gmail.com>
2018-02-17 16:56:58 -0500

    better validation

--- src/bin/friends_gunther.rs
+++ src/bin/friends_gunther.rs
@@ -1,15 +1,13 @@
 extern crate getopts;
 extern crate ross;
+extern crate regex;
 use std::fs::File;
 use std::io::BufReader;
 use std::io::BufRead;
-
-//use ross::io::fastq;
-//use ross::io::seq::Cleanable;
-
 use std::env;
 use getopts::Options;
 
+use regex::Regex;
 
 fn main(){
     let args: Vec<String> = env::args().collect();
@@ -21,6 +19,7 @@ fn main(){
     // Options specific to this script
     opts.optflag("","paired-end","The reads are interleaved paired-end");
     opts.optflag("","print","Print the reads as they are being validated (useful for unix pipes)");
+    opts.optflag("v","verbose","");
 
     let matches = opts.parse(&args[1..]).expect("ERROR: could not parse parameters");
     if matches.opt_present("help") {
@@ -36,11 +35,19 @@ fn main(){
         if matches.opt_present("paired-end") {
             4
         }else{
-                8
+            8
         }
     };
 
+    // save this option to avoid the overhead of calling
+    // opt_present many times in a loop
     let should_print=matches.opt_present("print");
+
+    // If there is a match on these, then mark invalid.
+    // In other words, we are looking for a pattern that
+    // is NOT the target seq or qual
+    let seq_regex = Regex::new(r"[^a-zA-Z]").expect("malformed seq regex");
+    let qual_regex= Regex::new(r"[^!-I]").expect("malformed qual regex");
 
     // TODO have a print buffer, something like 4096 bytes
 
@@ -55,17 +62,37 @@ fn main(){
         match i%lines_per_read {
             0=>{
                 in_middle_of_entry=true;
+                if line.chars().nth(0).unwrap() != '@' {
+                    panic!("ERROR: first character of the identifier is not @ in the line {}. Contents are:\n  {}",i,line); 
+                }
+            }
+            1=>{
+                if seq_regex.is_match(&line) {
+                    panic!("ERROR: there are characters that are not in the alphabet in line {}. Contents are:\n  {}",i,line);
+                }
+            }
+            2=>{
+                if line.chars().nth(0).unwrap() != '+' {
+                    panic!("ERROR: first character of the qual identifier is not + in the line {}. Contents are:\n  {}",i,line); 
+                }
             }
             3=>{
                 in_middle_of_entry=false;
+                if qual_regex.is_match(&line) {
+                    panic!("ERROR: there are characters that are not qual characters in line {}. Contents are:\n  {}",i,line);
+                }
             }
             _=>{
-                //panic!("INTERNAL ERROR");
+                panic!("INTERNAL ERROR");
             }
         }
     }
     if in_middle_of_entry {
         panic!("ERROR: incomplete fastq entry");
+    }
+
+    if matches.opt_present("verbose") {
+        ross::logmsg("These reads have been validated!");
     }
 }
 
