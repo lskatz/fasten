6a856edff0bc1b8eb4626df7f1e352394b58a378
Lee Katz <lskatz@gmail.com>
2018-02-22 21:25:31 -0500

    params fixed

--- src/bin/friends_monica.rs
+++ src/bin/friends_monica.rs
@@ -21,11 +21,16 @@ fn main(){
     // TODO put these options into ROSS to streamline.
     opts.optflag("h", "help", "Print this help menu.");
     opts.optopt("n","numcpus","Number of CPUs (default: 1)","INT");
-    // Options specific to this script
+
+    // script-specific options
+    opts.optopt("","min-length","Minimum length for each read in bp","INT");
+    opts.optopt("","min-avg-quality","Minimum average quality for each read","FLOAT");
+    opts.optopt("","min-trim-quality","Trim the edges of each read until a nucleotide of at least X quality is found","INT");
     let matches = opts.parse(&args[1..]).expect("ERROR: could not parse parameters");
 
     if matches.opt_present("h") {
         println!("{}", opts.usage(&opts.short_usage(&args[0])));
+        std::process::exit(0);
     }
 
     // defaults
@@ -38,6 +43,30 @@ fn main(){
     }
     if numcpus < 2 {
         numcpus=2;
+    }
+
+    let mut min_length :f32 = 0.0;
+    if matches.opt_present("min-length") {
+        min_length = matches.opt_str("min-length")
+            .expect("ERROR: could not read the minimum length parameter")
+            .parse()
+            .expect("ERROR: min-length is not an integer");
+    }
+
+    let mut min_avg_qual :f32 = 0.0;
+    if matches.opt_present("min-avg-quality") {
+        min_avg_qual = matches.opt_str("min-avg-quality")
+            .expect("ERROR: could not read the minimum average quality parameter")
+            .parse()
+            .expect("ERROR: min-avg-qual is not an integer");
+    }
+
+    let mut min_trim_qual :f32 = 0.0;
+    if matches.opt_present("min-trim-quality") {
+        min_trim_qual = matches.opt_str("min-trim-quality")
+            .expect("ERROR: could not read the minimum average quality parameter")
+            .parse()
+            .expect("ERROR: min-trim-qual is not an integer");
     }
 
     //let capacity = numcpus.pow(4) as u64;
@@ -57,9 +86,9 @@ fn main(){
                         eprintln!("Terminating signal!");
                         return;
                     }
-                    seq.thresholds.insert("min_length".to_string(),100.0);
-                    seq.thresholds.insert("min_avg_qual".to_string(),20.0);
-                    seq.thresholds.insert("min_trim_qual".to_string(),20.0);
+                    seq.thresholds.insert("min_length".to_string(),min_length);
+                    seq.thresholds.insert("min_avg_qual".to_string(),min_avg_qual);
+                    seq.thresholds.insert("min_trim_qual".to_string(),min_trim_qual);
                     seq.lower_ambiguity_q();
                     seq.trim();
                     if seq.is_high_quality() {
