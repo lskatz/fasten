fa93931469901142e5f267f4e804f5953945fcd3
Lee Katz - Aspen <gzu2@cdc.gov>
2018-03-21 16:32:32 -0400

    this is broken and I will fix it

--- src/bin/friends_monica.rs
+++ src/bin/friends_monica.rs
@@ -52,19 +52,116 @@ fn main(){
             .expect("ERROR: min-trim-qual is not an integer");
     }
 
+    let lines_per_read={
+        if matches.opt_present("paired-end") {
+            8
+        }else{
+            4
+        }
+    };
+
     // Read the file and send seqs to threads
     let my_file = File::open("/dev/stdin").expect("Could not open file");
     let my_buffer=BufReader::new(my_file);
-    let fastq_reader=fastq::FastqReader::new(my_buffer);
-    for mut seq in fastq_reader {
-        seq.thresholds.insert("min_length".to_string(),min_length);
-        seq.thresholds.insert("min_avg_qual".to_string(),min_avg_qual);
-        seq.thresholds.insert("min_trim_qual".to_string(),min_trim_qual);
-        seq.lower_ambiguity_q();
-        seq.trim();
-        if seq.is_high_quality() {
-           seq.print();
+    
+    let mut id1   :String = String::new();
+    let mut id2   :String = String::new();
+    let mut seq1  :String = String::new();
+    let mut seq2  :String = String::new();
+    let mut qual1 :String = String::new();
+    let mut qual2 :String = String::new();
+
+    for (i,line) in my_buffer.lines().enumerate() {
+        match num_lines % lines_per_read {
+            // read ID
+            0=>{
+                id1 = line;
+            }
+            4=>{
+                id2 = line;
+            }
+
+            // Sequence
+            1=>{
+                seq1 = line;
+            }
+            5=>{
+                seq2 = line;
+            }
+            // qual
+            3=>{
+                qual1 = line;
+            }
+            7=>{
+                qual2 = line;
+            }
+            _=>{}
+        }
+
+        // If both reads are set, then trim and clean
+        if (lines_per_read==4 && id1.len() > 0) || (lines_per_read==8 && id2.len() > 0) {
+            // trim
+            (seq1_trimmed,qual1_trimmed)=trim(seq1,qual1,min_trim_qual);
+            // filter
+            // print
+            println!("{}{}+\n{}",
+                     id1,seq1_trimmed,qual1_trimmed,
+                     );
+
+            if lines_per_read==8 {
+                (seq2_trimmed,qual2_trimmed)=trim(seq2,qual2,min_trim_qual);
+                println!("{}{}+\n{}",
+                         id2,seq2_trimmed,qual2_trimmed
+                         );
+            }
+
+            // reset
+            id1=    String::new();
+            id2=    String::new();
+            seq1=   String::new();
+            seq2=   String::new();
+            qual1=  String::new();
+            qual2=  String::new();
         }
     }
+
+}
+
+/// Trim the ends of reads with low quality
+fn trim(&seq: String, &qual: String, min_qual: u8) -> (String,String) {
+    let mut trim5 :u32=0;
+    let mut trim3 :u32=qual.len() as u32;
+
+    let offset_min_qual = min_qual + 33;
+    
+    // 5'
+    for qual in qual.chars(){
+        if qual as u8 < offset_min_qual {
+            trim5+=1;
+        } else {
+            break;
+        }
+    }
+
+    // 3'
+    for qual in qual.chars().rev() {
+        if qual as u8 < offset_min_qual {
+            trim3-=1;
+        } else {
+            break;
+        }
+    }
+
+    let mut new_seq :String;
+    let mut new_qual:String;
+    
+    if trim5 >= trim3 {
+        new_seq = String::new();
+        new_qual= String::new();
+    } else {
+        new_seq  =  seq[trim5..trim3].to_string();
+        new_qual = qual[trim5..trim3].to_string();
+    }
+    return(new_seq,new_qual);
 }
 
