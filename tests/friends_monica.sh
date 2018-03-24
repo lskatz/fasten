5cd65bff0b7cd382cd66c66a628a949d6299f5cc
Lee Katz <lskatz@gmail.com>
2018-03-24 14:17:47 -0400

    did I fix travis yet?

--- tests/friends_monica.sh
+++ tests/friends_monica.sh
@@ -10,7 +10,7 @@ if [ "$reads_not_cleaned" != "$original_reads" ]; then
   exit 1;
 fi
 
-one_cleaned_read=$(./target/debug/friends_monica --paired-end --min-trim-quality 30 --min-length 94 < testdata/four_reads.pe.fastq | friends_rachel --each-read | tail -n 1)
+one_cleaned_read=$(./target/debug/friends_monica --paired-end --min-trim-quality 30 --min-length 94 < testdata/four_reads.pe.fastq | ./target/debug/friends_rachel --each-read | tail -n 1)
 if [ "$one_cleaned_read" != $'read3/2\t94\t18.446808' ]; then
   echo "Test failed for cleaning reads"
   exit 1
