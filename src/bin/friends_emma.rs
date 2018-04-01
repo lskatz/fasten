30377024d050e425ca553fceba4ea58a95a702ac
Lee Katz <lskatz@gmail.com>
2018-04-01 17:57:51 -0400

    finally works, single end

--- src/bin/friends_emma.rs
+++ src/bin/friends_emma.rs
@@ -16,8 +16,8 @@ use ross::ross_base_options;
 // need this constant because the compiler had a problem
 // with the syntax 10.0.pow()
 const TEN: f32 = 10.0;
-const READ_SEPARATOR :&'static str = "~~~~";
-const READ_SEPARATOR_LENGTH :usize = 4;
+//const READ_SEPARATOR :&'static str = "~~~~";
+//const READ_SEPARATOR_LENGTH :usize = 4;
 
 fn main(){
     let args: Vec<String> = env::args().collect();
@@ -31,88 +31,114 @@ fn main(){
         std::process::exit(0);
     }
 
-    let lines_per_read={
-        if matches.opt_present("paired-end") {
-            8
-        }else{
-            4
-        }
-    };
+    let lines_per_read=4;
 
-    // Keep track of IDs
-    // id => seq
-    // id => qual
-    let mut seq_id    :HashMap<String,String>=HashMap::new();
-    //let mut qual_id   :HashMap<String,String>=HashMap::new();
-    // ID => count
-    let mut seq_count :HashMap<String,u32>   =HashMap::new();
-    // keep track of sequence pairing
-    // ID1 => ID2
-    let mut seq_pair  :HashMap<String,String>=HashMap::new();
-    // ID => vec![quality cigar String]
-    let mut qual      :HashMap<String,Vec<char>>=HashMap::new();
+    // seq => {seq2 => count}
+    let mut seq_count :HashMap<String, u32>   =HashMap::new();
+    // seq => {seq2 => vec![sequence of prob of errors]}
+    let mut seq_error_rate :HashMap<String, Vec<f32>> = HashMap::new();
 
-    // SEQ1~~~~SEQ2 => Vec<ID1~~~~ID2>
-    let mut identical_seq :HashMap<String,Vec<String>>=HashMap::new();
-
-    let mut current_id1  = String::new();
-    let mut current_id2  = String::new();
     let mut current_seq1 = String::new();
-    let mut current_seq2 = String::new();
+    //let mut current_seq2 = String::new();
+    //let mut current_qual1= String::new();
+    //let mut current_qual2= String::new();
 
     let my_file = File::open("/dev/stdin").expect("Could not open file");
     let my_buffer=BufReader::new(my_file);
     let mut line_counter =0;
     for line in my_buffer.lines() {
         match line_counter % lines_per_read {
-            0 => {
-                current_id1 = line.expect("Could not unwrap ID1");
-            }
-            4 => {
-                current_id2 = line.expect("Could not unwrap ID2");
-            }
             1 => {
                 current_seq1 = line.expect("Could not unwrap seq1");
-                let count = seq_count.entry(current_id1.clone()).or_insert(0);
+                // TODO transform current_seq1 to its minimum
+                let count = seq_count.entry(current_seq1.clone()).or_insert(0);
                 *count += 1;
-
-                seq_id.insert(current_id1.clone(),current_seq1.clone());
-            }
-            5 => {
-                let current_seq2 = line.expect("Could not unwrap seq2");
-                let count = seq_count.entry(current_id2.clone()).or_insert(0);
-                *count += 1;
-
-                seq_id.insert(current_id2.clone(),current_seq2.clone());
-
-                seq_pair.insert(current_id1.clone(),current_id2.clone());
             }
 
             // 3 and 7: the qual lines
             3 => {
-                let qual1 = line.expect("Could not unwrap qual1");
-                qual.insert(current_id1.clone(),qual1.chars().collect());
-                if lines_per_read == 4 {
-                    let mut seq_set :&Vec<String> = identical_seq.entry(current_seq1.clone()).or_insert(Vec::new());
-                    seq_set.push(current_id1.clone());
-                }
+                let qual1_string = line.expect("Could not unwrap qual1");
+                // If this sequence hasn't been seen yet,
+                // then instantiate the probabilities.
+                if !seq_error_rate.contains_key(&current_seq1) {
+                    let mut qual1_vec :Vec<f32> = Vec::new();
+                    for qual_char in qual1_string.chars() {
+                        let qual_int = qual_char as u8 as f32 - 33.0;
+                        let p :f32 = TEN.powf((-1.0 * qual_int)/TEN);
+                        qual1_vec.push(p);
+                    }
+                    seq_error_rate.insert(current_seq1.clone(), qual1_vec);
+                } 
+                // If this sequence has been seen yet, then
+                // start combining the error rates.
+                else {
+                    let qual1_vec = seq_error_rate.entry(current_seq1.clone()).or_insert(Vec::new());
 
-            }
-            7 => {
-                let qual2 = line.expect("Could not unwrap qual2");
-                qual.insert(current_id2.clone(),qual2.chars().collect());
+                    let these_errors = qual1_string.chars().map(|qual_char|{
+                        let qual_int = qual_char as u8 as f32 - 33.0;
+                        let p :f32 = TEN.powf((-1.0 * qual_int)/TEN);
+                        return p;
+                    }).collect();
+                    let new_qual = combine_error_vectors(&qual1_vec,&these_errors);
+                    *qual1_vec = new_qual;
+                }
             }
             _=>{}
         }
         line_counter += 1;
     }
 
-    // collapse the reads
-    for (id1,id2) in seq_pair {
-        println!("{} => {}",id1,id2);
+    let max_qual = 'I' as u8;
+    let min_qual = '#' as u8;
+
+    let mut seq_counter=0;
+    for (seq,combined_qual) in seq_error_rate {
+        seq_counter += 1;
+
+        // Make a new cigar line for quality
+        let mut qual_cigar = String::new();
+        for p in combined_qual {
+            let mut qual_recalc :f32 = -TEN * (p).log(TEN)+33.0;
+            // check for overflow before switching to u8
+            if qual_recalc.is_infinite() || qual_recalc > 255.0 {
+                qual_recalc = 255.0;
+            }
+
+            // switch to u8 and then the corresponding char
+            let mut qual_recalc_char = qual_recalc.floor() as u8 as char;
+            if (qual_recalc_char as u8) > max_qual {
+                qual_recalc_char = 'I';
+            }
+            // a reduction in quality is not expected... but just in case.
+            if (qual_recalc_char as u8) < min_qual {
+                qual_recalc_char = '#';
+            }
+            qual_cigar.push(qual_recalc_char);
+        }
+
+        println!("@{}\n{}\n+\n{}",seq_counter,seq,qual_cigar);
     }
 }
 
+
+fn combine_error_vectors(errors1 :&Vec<f32>, errors2: &Vec<f32>) -> Vec<f32> {
+    if errors1.len() != errors2.len() {
+        panic!("Lengths of error vectors do not match: {} and {}", errors1.len(), errors2.len());
+    }
+    let mut errors_iter2=errors2.iter();
+    let mut new_errors :Vec<f32> = Vec::new(); // TODO set length/capacity to errors.len()
+    for p1 in errors1 {
+        let p2 = errors_iter2.next().expect("ERROR: could not get the error probability from the second read");
+        new_errors.push(p1 * p2);
+    }
+    return new_errors;
+}
+
+// TODO a function that returns the 'min-seq' which is the
+// sequence that comes first alphabetically when compared
+// with its revcom
+
+#[allow(dead_code)]
 fn recalculate_qual(qual_str: &str, count: u32) -> String {
     let mut qual_out = String::new();
 
