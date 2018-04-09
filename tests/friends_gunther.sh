c6567ef93e2e77599661e781d5306095790d362d
Lee Katz <lskatz@gmail.com>
2018-04-08 22:51:16 -0400

    routing the panic message to dev/null

--- tests/friends_gunther.sh
+++ tests/friends_gunther.sh
@@ -9,9 +9,8 @@ if [ $? -gt 0 ]; then
   exit 1
 fi
 
-echo "Purposefully triggering an error..."
 set +e
-head -n 15 $INPUT | ./target/debug/friends_gunther --paired-end --verbose
+head -n 15 $INPUT | ./target/debug/friends_gunther --paired-end --verbose 2>/dev/null
 if [ $? -eq 0 ]; then
   echo "Test failed for marking a bad set of reads"
   exit 1
