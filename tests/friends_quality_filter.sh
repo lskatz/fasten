2ecbb57e58e4979bfaa1fc3eace59eea6882cfcc
Lee Katz <lskatz@gmail.com>
2018-04-14 18:12:44 -0400

    quality filter test

---
+++ tests/friends_quality_filter.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+INPUT="testdata/four_reads.pe.fastq"
+reads=$(cat $INPUT)
+masked=$(./target/debug/friends_quality_filter --max-quality 20 < testdata/four_reads.pe.fastq)
+
+num_Ns=$(
+  ./target/debug/friends_quality_filter --max-quality 20 < testdata/four_reads.pe.fastq | \
+    perl -lane '
+      for $char(split(//,$_)){
+        $N++ if($char eq "N");
+      } END{print $N}
+    '
+  );
+
+if [ "$num_Ns" -ne 393 ]; then
+  echo "ERROR: number of expected bases were not masked"
+  exit 1
+fi
+
+echo "$0 passed!"
