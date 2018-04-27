2e46826ec376d4d61bfcca068bafeb36c3a63789
Lee Katz <lskatz@gmail.com>
2018-04-26 21:46:14 -0400

    pe tests continue...

--- src/bin/fasten_pe.rs
+++ src/bin/fasten_pe.rs
@@ -16,11 +16,8 @@ fn main() {
     opts.optopt("c","check-first","How many deflines to check to make sure the input is paired-end","INT");
     let matches = opts.parse(&args[1..]).expect("ERROR: could not parse parameters");
     
-    // @.../1
-    let slash_r1r2_regex = Regex::new(r"(.+)/([12])$").expect("malformed qual regex");
-
     if matches.opt_present("help") {
-        println!("Determine paired-end-ness in an interleaved file. Currently only checks deflines for the /1 and /2 format\n{}", 
+        println!("Determine paired-end-ness in an interleaved file. Exit code of 0 indicates PE. Exit code > 0 indicates SE.\n{}", 
                  opts.usage(&opts.short_usage(&args[0]))
                 );
         std::process::exit(0);
@@ -46,6 +43,8 @@ fn main() {
     // and compare them in several functions that test for
     // IDs. If any test returns true, then we can say that
     // it is paired-end input.
+    let mut id1_vec :Vec<String>=Vec::new();
+    let mut id2_vec :Vec<String>=Vec::new();
     let my_file = File::open("/dev/stdin").expect("Could not open file");
     let my_buffer=BufReader::new(my_file);
     let mut lines = my_buffer.lines();
@@ -64,45 +63,90 @@ fn main() {
                 .expect("ERROR getting next line in R2. This is not a paired end file.")
                 .expect("ERROR parsing next line in R2");
         }
+        id1_vec.push(id1);
+        id2_vec.push(id2);
 
         pairs_counter+=1;
-
-        let caps1 = slash_r1r2_regex.captures(&id1)
-            .expect("ERROR: could not regex against id1");
-        let caps2 = slash_r1r2_regex.captures(&id2).
-            expect("ERROR: could not regex against id2");
-
-        // Make sure the base name matches
-        if caps1[1] != caps2[1] {
-            let mut msg = "ID1 does not match ID2 on pair ".to_string();
-            msg.push_str(&pairs_counter.to_string());
-            msg.push_str("\n");
-            msg.push_str(&id1);
-            msg.push_str(" vs ");
-            msg.push_str(&id2);
-            logmsg(&msg);
-            std::process::exit(1);
-        }
-        // Make sure there is a 1/2 combo
-        if &caps1[2] != "1" || &caps2[2] != "2" {
-            let mut msg = "/1 is not followed by /2 on pair ".to_string();
-            msg.push_str(&pairs_counter.to_string());
-            msg.push_str("\n");
-            msg.push_str(&id1);
-            msg.push_str(" vs ");
-            msg.push_str(&id2);
-            logmsg(&msg);
-            std::process::exit(1);
-        }
-
         if pairs_counter >= check_first {
             break;
         }
-
     }
 
+    // For every function we can think of, throw the ID
+    // vectors against them. If any of them return >0,
+    // then we have a positive
+    let mut is_paired_end :u8 = 0;
+    is_paired_end += is_paired_end_slash12(&id1_vec, &id2_vec);
+    //is_paired_end += is_paired_end_miseq(&id1_vec, &id2_vec);
+
+    // If this is not paired end, return a nonzero
+    if is_paired_end == 0 {
+        if matches.opt_present("verbose") {
+            logmsg("I do not think this is interleaved paired end");
+        }
+        std::process::exit(1);
+    }
+    
     if matches.opt_present("verbose") {
         logmsg("The fastq input seems to be interleaved paired-end");
     }
+
 }
+
+fn is_paired_end_slash12(id1_ref: &Vec<String>, id2_ref: &Vec<String>) -> u8 {
+
+    // We are using iterators and want to reset them
+    let id1_vec = id1_ref.clone();
+    let id2_vec = id2_ref.clone();
+    
+    // @.../1
+    let slash_r1r2_regex = Regex::new(r"(.+)/([12])$").expect("malformed qual regex");
+
+    let mut id2_iter = id2_vec.iter();
+    for id1 in id1_vec.iter(){
+        let id2 = id2_iter.next()
+            .expect("ERROR getting next id2");
+
+        let caps1_result = slash_r1r2_regex.captures(&id1);
+        let caps2_result = slash_r1r2_regex.captures(&id2);
+
+        // Test for whether the regex matched
+        if caps1_result.is_none() || caps2_result.is_none() {
+            return 0;
+        }
+
+        // If we have a match then get the captured values
+        let caps1 = caps1_result
+            .expect("ERROR: could not regex against id1");
+        let caps2 = caps2_result
+            .expect("ERROR: could not regex against id2");
+
+        // Make sure the base name matches
+        if caps1[1] != caps2[1] {
+            return 0;
+        }
+        // Make sure there is a 1/2 combo
+        if &caps1[2] != "1" || &caps2[2] != "2" {
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+/*
+ * http://support.illumina.com/content/dam/illumina-support/help/BaseSpaceHelp_v2/Content/Vault/Informatics/Sequencing_Analysis/BS/swSEQ_mBS_FASTQFiles.htm
+fn is_paired_end_miseq(id1_ref: &Vec<String>, id2_ref: &Vec<String>) -> u8 {
+    // We are using iterators and want to reset them
+    let id1_vec = id1_ref.clone();
+    let id2_vec = id2_ref.clone();
+
+    for id1 in id1_vec.iter(){
+        let id2 = id2_iter.next()
+            .expect("ERROR getting next id2");
+
+
+        let mut split_id1 = id1.split(":");
+        let mut split_id2 = id2.split(":");
+*/
 
