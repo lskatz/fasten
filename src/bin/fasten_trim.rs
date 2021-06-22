c518851d8acb124e470c0ca4dd8bdd7481ad2a48
Lee Katz <lskatz@gmail.com>
2021-06-22 13:19:51 -0400

    streamlining trim script to use Seq object

--- src/bin/fasten_trim.rs
+++ src/bin/fasten_trim.rs
@@ -5,14 +5,17 @@ extern crate threadpool;
 
 use std::fs::File;
 use std::io::BufReader;
-use std::io::BufRead;
 use std::env;
-use std::thread;
 use std::cmp::min;
+
 use threadpool::ThreadPool;
+use std::sync::mpsc::channel;
 
 use fasten::fasten_base_options;
 use fasten::logmsg;
+use fasten::io::fastq;
+//use fasten::io::seq::Cleanable;
+use fasten::io::seq::Seq;
 
 fn main(){
     let args: Vec<String> = env::args().collect();
@@ -28,9 +31,10 @@ fn main(){
         println!("Blunt-end trims using 0-based coordinates\n{}", opts.usage(&opts.short_usage(&args[0])));
         std::process::exit(0);
     }
-    if matches.opt_present("paired-end") {
-        logmsg("WARNING: --paired-end is not utilized in this script");
-    }
+
+    let (tx, rx):(std::sync::mpsc::Sender<String>,std::sync::mpsc::Receiver<String>) = channel();
+
+    let paired_end:bool = matches.opt_present("paired-end");
 
     let first_base:usize ={
         if matches.opt_present("first-base") {
@@ -73,43 +77,56 @@ fn main(){
     let pool = ThreadPool::with_name("worker".into(), num_cpus);
 
     // Read from stdin
-    let filename = "/dev/stdin";
-    
-    // read the file
-    let my_file = File::open(&filename).expect("Could not open file");
+    let my_file = File::open("/dev/stdin").expect("Could not open file");
     let my_buffer=BufReader::new(my_file);
-    let mut num_lines=0;
-    let mut lines_buffer:Vec<String> = vec![];
-
-    for line_result in my_buffer.lines() {
-        num_lines+=1;
-
-        let line = line_result.expect("Error reading line");
-        &lines_buffer.push(line);
-
-        if num_lines % 888888 == 0 {
-          eprintln!("{} lines queued", &num_lines);
-          // grab the buffer into a buffer for the thread
-          // and then empty the main buffer
-          let mut sub_lines_buffer = lines_buffer.clone();
-          lines_buffer = vec![];
-          pool.execute(move|| {
-            logmsg("enqueuing");
-            trim_worker(&mut sub_lines_buffer, first_base, last_base);
-          });
+    let fastq_reader = fastq::FastqReader::new(my_buffer);
+    let mut fastq_iter   = fastq_reader.into_iter();
+    while let Some(seq) = fastq_iter.next() {
+        // start off an array of seq objects that will 
+        // contain R1 (and R2).
+        let mut seqs:Vec<Seq> = vec![seq];
+        if paired_end {
+          seqs.push(
+            fastq_iter.next()
+              .expect("Tried to get the second sequence in a pair but ran into an error")
+          );
         }
+
+        // Send this single end or paired end to the queue
+        let tx2 = tx.clone();
+        pool.execute(move|| {
+          trim_worker(seqs, first_base, last_base, tx2);
+        });
     }
-    // One last time with the remaining buffer.
-    // Need to queue it so that it doesn't make the main 
-    // thread do extra work.
-    pool.execute(move|| {
-      trim_worker(&mut lines_buffer, first_base, last_base);
-    });
 
     pool.join();
+    drop(tx); // disconnects the channel
+
+    let receiver = rx.iter();
+    for entry in receiver {
+      println!("{}",entry);
+    }
+
 }
 
-fn trim_worker(sub_lines_buffer:&mut Vec<String>, first_base:usize, last_base:usize ){
+fn trim_worker(seqs:Vec<Seq>, first_base:usize, last_base:usize, tx:std::sync::mpsc::Sender<String> ){
+
+  for seq in seqs{
+    // The last position is either the last_base parameter
+    // or the last position in the string, whichever is less.
+    let last_base_tmp = min(seq.seq.len(), last_base);
+
+    let sequence = &seq.seq[first_base..last_base_tmp];
+    let quality  = &seq.qual[first_base..last_base_tmp];
+
+    let trimmed = format!("{}\n{}\n+\n{}", seq.id, sequence, quality);
+    tx.send(trimmed).unwrap();
+  }
+}
+  
+
+/*
+fn trim_worker_old(sub_lines_buffer:&mut Vec<String>, first_base:usize, last_base:usize, tx:std::sync::mpsc::Sender<String> ){
   let this_thread = thread::current();
   let _tid = this_thread.id(); // for debugging
 
@@ -127,12 +144,15 @@ fn trim_worker(sub_lines_buffer:&mut Vec<String>, first_base:usize, last_base:us
     seq  = String::from(seq);
     qual = String::from(qual);
 
-    println!("{}\n{}\n{}\n{}",
+    let entry = format!("{}\n{}\n{}\n{}",
       id, &seq[first_base..last_base_tmp], plus, &qual[first_base..last_base_tmp]
     );
+
+    tx.send(entry).unwrap();
 
   }
   //eprintln!("{:?} finished {}", &_tid, &num_lines);
 }
 
+*/
 
