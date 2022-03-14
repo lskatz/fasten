12ef7dfbddcdb113119f1ebdc51a02dcbad5f83d
Lee Katz - Aspen <gzu2@cdc.gov>
2022-03-14 14:11:55 -0400

    more docs

--- src/bin/fasten_kmer.rs
+++ src/bin/fasten_kmer.rs
@@ -1,3 +1,26 @@
+//! Counts kmers.
+//! Each line is a kmer with two columns separated by tab: kmer, count
+
+//! # Examples
+
+//! ```bash
+//! cat testdata/four_reads.fastq | fasten_kmer -k 15 > 15mers.tsv
+//! ```
+
+//! # Usage
+
+//! ```text
+//! Usage: fasten_kmer [-h] [-n INT] [-p] [-v] [-k INT]
+
+//! Options:
+//!    -h, --help          Print this help menu.
+//!    -n, --numcpus INT   Number of CPUs (default: 1)
+//!    -p, --paired-end    The input reads are interleaved paired-end
+//!    -v, --verbose       Print more status messages
+//!    -k, --kmer-length INT
+//!                        The size of the kmer
+//! ```
+    
 extern crate fasten;
 extern crate statistical;
 extern crate getopts;
