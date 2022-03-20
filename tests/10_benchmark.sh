6d48d724adfc67168ac506469a06b6cb56426c3c
Lee Katz - Aspen <gzu2@cdc.gov>
2022-03-20 07:40:00 -0400

    more benchmarking which also includes seqtk

--- tests/10_benchmark.sh
+++ tests/10_benchmark.sh
@@ -2,6 +2,15 @@
 
 set -e
 
+# Hyperfine parameters
+# Locally, just run one time per test but in the cloud, boost it to ten
+num_runs=1
+if [[ ! -z "$CI" ]]; then
+  num_runs=10
+fi
+# output directory for markdown files
+reportsDir="hyperfine"
+
 thisDir=$(dirname $(realpath $0))
 
 R1="$thisDir/../testdata/R1.fastq"
@@ -27,9 +36,31 @@ done > $large_R2
 cat $R1 $R2 | fasten_shuffle | gzip -c9 > $large_interleaved
 ls -lhS $large_interleaved $R1 $R2
 
+hyperfine --warmup 2 --shell $SHELL --runs $num_runs \
+  -n "Fasten sample" "cat $large_R1 | fasten_sample --frequency 0.1" \
+  -n "Seqtk sample" "seqtk seq -f 0.1 $large_R1"; 
+
+# Convert fastq=>fasta.
+# Set the width to really long on fq2fa to match the output exactly
+hyperfine --warmup 2 --shell $SHELL --runs $num_runs \
+  -n "Fasten convert" "zcat $large_sorted | fasten_convert -i fastq -o fasta " \
+  -n "Seqkit fq2fa -w 1000" "zcat $large_sorted | seqkit fq2fa " \
+  -n "Seqtk seq -A" "seqtk seq -A $large_sorted"
+
+# get first 100 reads for any fastq file
+hyperfine --warmup 2 --shell $SHELL --runs $num_runs --export-markdown $(mktemp --tmpdir=$reportsDir hyperfine.XXXXXX.md) \
+  -n "seqkit head" "cat $large_R1 | seqkit head --number 100" \
+  -n "fasten_straighten" "cat $large_R1 | fasten_straighten | head -n 400"
+
+# Grep for CCCC
+pattern="CCCC"
+hyperfine --warmup 2 --shell $SHELL --runs $num_runs \
+  -n "seqkit grep" "cat $large_R1 | seqkit grep --by-seq --pattern 'CCCC' > /dev/null" \
+  -n "fasten_regex" "cat $large_R1 | fasten_regex --regex 'CCCC' --which SEQ --numcpus 12 > /dev/null"
+
 # Fasten_progress time trial
-hyperfine --warmup 2 --shell /bin/bash --runs 10 \
-  -n "Basic shuffle" "cat $large_R1 $large_R2 | fasten_shuffle > /dev/null" \
+hyperfine --warmup 2 --shell $SHELL --runs $num_runs \
+  -n "Basic fasten_shuffle" "cat $large_R1 $large_R2 | fasten_shuffle > /dev/null" \
   -n "Shuffle with progress bar" "cat $large_R1 $large_R2 | fasten_progress --print 2>/dev/null | fasten_shuffle > /dev/null" \
   -n "Shuffle with wc -l" "cat $large_R1 $large_R2 | tee >(wc -l >&2) | fasten_shuffle > /dev/null"
 
@@ -37,17 +68,11 @@ hyperfine --warmup 2 --shell /bin/bash --runs 10 \
 zcat $large_interleaved | fasten_sort --sort-by GC --paired-end | gzip -c > $large_sorted
 ls -lh $large_interleaved $large_sorted
 
-# Convert fastq=>fasta.
-# Set the width to really long on fq2fa to match the output exactly
-hyperfine --warmup 2 --shell /bin/bash --runs 10 \
-  -n "Fasten convert" "zcat $large_sorted | fasten_convert -i fastq -o fasta > /dev/null" \
-  -n "Seqkit fq2fa -w 1000" "zcat $large_sorted | seqkit fq2fa > /dev/null"
-
 # sorting
-hyperfine --warmup 2 --shell /bin/bash --runs 10 \
+hyperfine --warmup 2 --shell $SHELL --runs $num_runs \
   -n "Fasten sort id" "zcat $large_R1 | fasten_sort --sort-by id > /dev/null" \
   -n "Seqkit sort id" "zcat $large_R1 | seqkit sort > /dev/null"
-hyperfine --warmup 2 --shell /bin/bash --runs 10 \
+hyperfine --warmup 2 --shell $SHELL --runs $num_runs \
   -n "Fasten sort seq" "zcat $large_R1 | fasten_sort --sort-by seq > /dev/null" \
   -n "Seqkit sort seq" "zcat $large_R1 | seqkit sort --by-seq > /dev/null"
 
