886a000f8f571323003bcaf705ad5b68d9374c62
Lee Katz - Aspen <gzu2@cdc.gov>
2023-10-11 19:36:08 -0400

    errors and warnings

--- src/bin/fasten_repair.rs
+++ src/bin/fasten_repair.rs
@@ -54,6 +54,7 @@ use std::collections::HashMap;
 
 use fasten::fasten_base_options;
 use fasten::fasten_base_options_matches;
+use fasten::logmsg;
 
 fn main(){
     let mut opts = fasten_base_options();
@@ -119,14 +120,14 @@ fn repair_reads(paired_end:bool, min_length: usize, min_qual: f32, remove_info: 
     let mut plus = String::new();
     let mut qual = String::new();
 
-    let mut i = 0;
+    //let mut i = 0;
     loop{
 
         id.clear();
         if my_buffer.read_line(&mut id).expect("Cannot read new line") == 0 {
             break;
         }
-
+        
         seq.clear();
         my_buffer.read_line(&mut seq).expect("ERROR: failed to read 'seq' line");
         plus.clear();
@@ -138,92 +139,169 @@ fn repair_reads(paired_end:bool, min_length: usize, min_qual: f32, remove_info: 
         plus = plus.trim().to_string();
         qual = qual.trim().to_string();
 
-        let mut identifier = String::new();
-        let mut f:HashMap<&str, &str> = HashMap::new();
-        for field in id.split_whitespace() {
-            match field.find(":") {
-                None => {
-                    identifier.push_str(&field);
-                    continue;
-                },
-                Some(_) => {},
-            };
-            let mut key_value = field.split(':');
-            let key   :&str = key_value.next().expect("key not found");
-            let value :&str = key_value.next().expect("value not found");
-            f.insert(key, value);
+        let (r1, is_r1_good, err1):(String, bool, String) = repair_one_read(id.clone(), seq.clone(), plus.clone(), qual.clone(), should_repair, min_length, min_qual, remove_info);
+        //i += 4;
+
+        let mut is_r2_good = true;
+        let mut r2 = "".to_string();
+        let mut err2 = "".to_string();
+        if paired_end {
+            let r1_id = id.clone();
+            id.clear();
+            if my_buffer.read_line(&mut id).expect("Cannot read new line") == 0 {
+                panic!("ERROR: paired end expected but not found after R1 {}", r1_id);
+            }
+            
+            seq.clear();
+            my_buffer.read_line(&mut seq).expect("ERROR: failed to read 'seq' line");
+            plus.clear();
+            my_buffer.read_line(&mut plus).expect("ERROR: failed to read 'plus' line");
+            qual.clear();
+            my_buffer.read_line(&mut qual).expect("ERROR: failed to read 'qual' line");
+            id   = id.trim().to_string();
+            seq  = seq.trim().to_string();
+            plus = plus.trim().to_string();
+            qual = qual.trim().to_string();
+
+            (r2, is_r2_good, err2) = repair_one_read(id.clone(), seq.clone(), plus.clone(), qual.clone(), should_repair, min_length, min_qual, remove_info);
         }
 
-        // get some variables out of the hash
-        let seq_length  :usize = f.entry("seq-length").or_insert("0").parse::<usize>().unwrap();
-        let qual_length :usize = f.entry("qual-length").or_insert("0").parse::<usize>().unwrap();
-        let avg_qual    :f32   = f.entry("avg-qual").or_insert("0").parse::<f32>().unwrap();
-        let seq_invalid_chars :&str  = f.entry("seq-invalid-chars").or_insert("");
-        let qual_invalid_chars :&str = f.entry("qual-invalid-chars").or_insert("");
-        let _read_pair :u8 = f.entry("read-pair").or_insert("1").parse::<u8>().unwrap(); // either 1 or 2
-        // these are either 1 (true) or 0 (false)
-        let id_at :u8 = f.entry("id-at").or_insert("0").parse::<u8>().unwrap();
-        let id_plus :u8 = f.entry("id-plus").or_insert("0").parse::<u8>().unwrap();
+        if is_r1_good && is_r2_good{
+            // Start with R1 for printing.
+            let mut to_print = r1.clone();
+            // If we're looking at a paired end, then add R2 to R1 for printing.
+            if paired_end {
+                to_print.push_str("\n");
+                to_print.push_str(&r2);
+            } 
+            println!("{}", to_print);
 
-        // Check seq length and qual length
-        if seq_length != qual_length {
-            if should_repair {
-                let new_length :usize = *vec![seq_length, qual_length].iter().min().unwrap();
-                seq  = seq[..new_length].to_string();
-                qual = qual[..new_length].to_string();
-                eprintln!("Repaired sequence length for {}", &id);
-            } else {
-                panic!("ERROR: seq length({}) did not match qual length({}) on seqid {}", &seq_length, &qual_length, &id);
+            // If R1 and R2 are good, then any "errors" are warnings.
+            // Print the warnings.
+            if err1.as_str() != "" {
+                logmsg(format!("WARNING(s) on R1: {}", err1));
+            }
+            if err2.as_str() != "" {
+                logmsg(format!("WARNING(s) on R2: {}", err2));
+            }
+        } else {
+            
+            // Print the errors.
+            if err1.as_str() != "" {
+                logmsg(format!("ERROR(s) on R1: {}", err1));
+            }
+            if err2.as_str() != "" {
+                logmsg(format!("ERROR(s) on R2: {}", err2));
             }
         }
-        if seq_length < min_length {
-            // TODO skip this sequence
-            //   TODO consider whether this is paired-end
-            panic!("ERROR: seq length({}) is less than min length specified ({})", &seq_length, &min_length);
-        }
-        // Check quality score
-        if avg_qual < min_qual {
-            panic!("ERROR: average quality ({}) is less than min quality ({})", &avg_qual, &min_qual);
-        }
+    }
+}
 
-        // check key seq-invalid-chars
-        if seq_invalid_chars != "" {
-            panic!("ERROR: invalid seq characters found in {}", &id);
-        }
-        // check key qual-invalid-chars
-        if qual_invalid_chars != "" {
-            panic!("ERROR: invalid qual characters found in {}", &id);
-        }
+/// Repair exactly one read
+fn repair_one_read(mut id:String, mut seq:String, plus:String, mut qual:String, should_repair:bool, min_length: usize, min_qual: f32, remove_info: bool) -> (String, bool, String) {
+    // Eventual error message if any
+    let mut error = String::new();
+    let mut num_errors = 0;
 
-        // check key id-at
-        if id_at < 1 {
-            panic!("ERROR: no @ found at position 1 on line 1 for {}", &id);
-        }
-        // check key id-plus 
-        if id_plus < 1 {
-            panic!("ERROR: no + found at position 1 on line 3 for {}", &id);
-        }
-
-        i += 4;
-
-        let mut denominator = 4;
-        if paired_end {
-            denominator = 8;
-        }
-        if (i % denominator) != 0 {
-            panic!("ERROR: incomplete lines in this latest entry with ID {}", &id);
-        }
-        
-        // TODO check that read1/read2 alternate
-        
-        // if the user requests, we can remove all fasten_inspect information
-        if remove_info {
-            id = identifier.clone();
-        }
-        println!("{}\n{}\n{}\n{}", &id, &seq, &plus, &qual);
+    // The eventual sequence identifier with fasten_inspect info or not
+    let mut identifier = String::new();
+    // Information about the read from the defline
+    let mut f:HashMap<&str, &str> = HashMap::new();
+    // Get that information from the defline
+    for field in id.split_whitespace() {
+        match field.find(":") {
+            None => {
+                identifier.push_str(&field);
+                continue;
+            },
+            Some(_) => {},
+        };
+        let mut key_value = field.split(':');
+        let key   :&str = key_value.next().expect("key not found");
+        let value :&str = key_value.next().expect("value not found");
+        f.insert(key, value);
     }
 
-    // TODO if paired end, make sure we end on second read
+    // get some variables out of the hash
+    let seq_length  :usize = f.entry("seq-length").or_insert("0").parse::<usize>().unwrap();
+    let qual_length :usize = f.entry("qual-length").or_insert("0").parse::<usize>().unwrap();
+    let avg_qual    :f32   = f.entry("avg-qual").or_insert("0").parse::<f32>().unwrap();
+    let seq_invalid_chars :&str  = f.entry("seq-invalid-chars").or_insert("");
+    let qual_invalid_chars :&str = f.entry("qual-invalid-chars").or_insert("");
+    let _read_pair :u8 = f.entry("read-pair").or_insert("1").parse::<u8>().unwrap(); // either 1 or 2
+    // these are either 1 (true) or 0 (false)
+    let id_at :u8 = f.entry("id-at").or_insert("0").parse::<u8>().unwrap();
+    let id_plus :u8 = f.entry("id-plus").or_insert("0").parse::<u8>().unwrap();
 
+    // Check seq length and qual length
+    if seq_length != qual_length {
+        if should_repair {
+            let new_length :usize = *vec![seq_length, qual_length].iter().min().unwrap();
+            seq  = seq[..new_length].to_string();
+            qual = qual[..new_length].to_string();
+            error.push_str(
+                &format!("Repaired sequence length for {}\n", &id)
+            );
+            // Don't count this as an actual error and so don't increment.
+        } else {
+            panic!("ERROR: seq length({}) did not match qual length({}) on seqid {}\n", &seq_length, &qual_length, &id);
+        }
+    }
+    if seq_length < min_length {
+        error.push_str(
+            &format!("ERROR: seq length({}) is less than min length specified ({})\n", &seq_length, &min_length)
+        );
+        num_errors += 1;
+    }
+    // Check quality score
+    if avg_qual < min_qual {
+        error.push_str(
+            &format!("ERROR: average quality ({}) is less than min quality ({})\n", &avg_qual, &min_qual)
+        );
+        num_errors += 1;
+    }
+
+    // check key seq-invalid-chars
+    if seq_invalid_chars != "" {
+        error.push_str(
+            &format!("ERROR: invalid seq characters found in {}\n", &id)
+        );
+        num_errors += 1;
+    }
+    // check key qual-invalid-chars
+    if qual_invalid_chars != "" {
+        error.push_str(
+            &format!("ERROR: invalid qual characters found in {}\n", &id)
+        );
+        num_errors += 1;
+    }
+
+    // check key id-at
+    if id_at < 1 {
+        error.push_str(
+            &format!("ERROR: no @ found at position 1 on line 1 for {}\n", &id)
+        );    
+        num_errors += 1;
+    }
+    // check key id-plus 
+    if id_plus < 1 {
+        error.push_str(
+            &format!("ERROR: no + found at position 1 on line 3 for {}\n", &id)
+        );
+        num_errors += 1;
+    }
+            
+    // if the user requests, we can remove all fasten_inspect information
+    if remove_info {
+        id = identifier.clone();
+    }
+    
+    let entry :String = format!("{}\n{}\n{}\n{}", &id, &seq, &plus, &qual);
+
+    let mut is_good = true;
+    if num_errors > 0 {
+        is_good = false;
+    }
+    return (entry, is_good, error);
 }
 
