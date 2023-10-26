438a3d18db1df73ab437041bee6a3c12c18f9fe1
Lee Katz - Aspen <gzu2@cdc.gov>
2023-10-26 09:19:45 -0400

    update benchmark

--- tests/10_benchmark.sh
+++ tests/10_benchmark.sh
@@ -56,41 +56,41 @@ hyperfine --export-json=$reportsDir/progress.json --warmup 2 --shell $SHELL --ru
   -n "Shuffle with progress bar before" "cat $large_R1 $large_R2 | fasten_progress --print 2>/dev/null | fasten_shuffle > /dev/null" \
   -n "Shuffle with progress bar after" "cat $large_R1 $large_R2 | fasten_shuffle | fasten_progress 2>/dev/null > /dev/null" 
 
-plot_whisker.py --title "Interleave reads with or without a progress meter" --labels without,before,after --output $reportsDir/progress.json.png $reportsDir/progress.json
+plot_whisker.py --title "Interleave reads with or without a progress meter (reps=$num_runs)" --labels without,before,after --output $reportsDir/progress.json.png $reportsDir/progress.json
 
 hyperfine --export-json=$reportsDir/sample.json --warmup 2 --shell $SHELL --runs $num_runs \
   -n "Fasten sample" "cat $large_R1 | fasten_sample --frequency 0.1" \
   -n "Seqtk sample" "seqtk seq -f 0.1 $large_R1"; 
 
-plot_whisker.py --title "subsample reads" --labels "fasten sample, seqtk sample" --output $reportsDir/sample.json.png $reportsDir/sample.json
+plot_whisker.py --title "subsample reads" --labels "fasten sample, seqtk sample (reps=$num_runs)" --output $reportsDir/sample.json.png $reportsDir/sample.json
 
 # Convert fastq=>fasta.
 # Set the width to really long on fq2fa to match the output exactly
 hyperfine --export-json=$reportsDir/convertToFasta.json --warmup 2 --shell $SHELL --runs $num_runs \
   -n "Fasten convert" "zcat $large_sorted | fasten_convert -i fastq -o fasta " \
-  -n "Seqkit fq2fa -w 1000" "zcat $large_sorted | seqkit fq2fa "
+  -n "Seqkit fq2fa -w 0" "zcat $large_sorted | seqkit fq2fa "
   #-n "Seqtk seq -A" "seqtk seq -A $large_sorted"
 
-plot_whisker.py --title "Converting fastq to fasta" --output $reportsDir/convertToFasta.json.png $reportsDir/convertToFasta.json
+plot_whisker.py --title "Converting fastq to fasta (reps=$num_runs)" --output $reportsDir/convertToFasta.json.png $reportsDir/convertToFasta.json
 
 # get first 100 reads for any fastq file
 hyperfine --export-json=$reportsDir/straighten.json --warmup 2 --shell $SHELL --runs $num_runs \
-  -n "seqkit head" "cat $large_R1 | seqkit head --number 100" \
   -n "fasten_straighten" "cat $large_R1 | fasten_straighten | head -n 400" \
+  -n "seqkit head" "cat $large_R1 | seqkit head --number 100" \
   -n "seqtk seq straighten" "seqtk seq -l0 $large_R1 | head -n 400"
 
-plot_whisker.py --title "Get first 100 reads from fastq" --output $reportsDir/straighten.json.png $reportsDir/straighten.json
+plot_whisker.py --title "Get first 100 reads from fastq (reps=$num_runs)" --output $reportsDir/straighten.json.png $reportsDir/straighten.json
 
 # Grep for seq 
 pattern="CCCC"
 pattern2="ATG"
 hyperfine --export-json=$reportsDir/regex.json --warmup 2 --shell $SHELL --runs $num_runs \
-  -n "seqkit grep $pattern" "cat $large_R1 | seqkit grep --by-seq --pattern '$pattern' > /dev/null" \
-  -n "seqkit grep $pattern2" "cat $large_R1 | seqkit grep --by-seq --pattern '$pattern2' > /dev/null" \
   -n "fasten_regex $pattern" "cat $large_R1 | fasten_regex --regex '$pattern' --which SEQ > /dev/null" \
-  -n "fasten_regex $pattern2" "cat $large_R1 | fasten_regex --regex '$pattern2' --which SEQ > /dev/null"
+  -n "fasten_regex $pattern2" "cat $large_R1 | fasten_regex --regex '$pattern2' --which SEQ > /dev/null" \
+  -n "seqkit grep $pattern" "cat $large_R1 | seqkit grep --by-seq --pattern '$pattern' > /dev/null" \
+  -n "seqkit grep $pattern2" "cat $large_R1 | seqkit grep --by-seq --pattern '$pattern2' > /dev/null" 
 
-plot_whisker.py --title "Regex on a sequence" --output $reportsDir/regex.json.png $reportsDir/regex.json
+plot_whisker.py --title "Regex on a sequence (reps=$num_runs)" --output $reportsDir/regex.json.png $reportsDir/regex.json
 
 # Fasten sort file size
 zcat $large_interleaved | fasten_sort --sort-by GC --paired-end | gzip -c > $large_sorted
@@ -103,5 +103,5 @@ hyperfine --export-json=$reportsDir/sort.json --warmup 2 --shell $SHELL --runs $
   -n "Seqkit sort id" "zcat $large_R1 | seqkit sort > /dev/null" \
   -n "Seqkit sort seq" "zcat $large_R1 | seqkit sort --by-seq > /dev/null"
 
-plot_whisker.py --title "Sort sequences" --output $reportsDir/sort.json.png $reportsDir/sort.json
+plot_whisker.py --title "Sort sequences (reps=$num_runs)" --output $reportsDir/sort.json.png $reportsDir/sort.json
 
