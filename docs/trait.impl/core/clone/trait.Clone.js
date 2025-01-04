3d2f9644a767f6479d5ff297ff049e3da4f7965f
Lee Katz - Aspen <gzu2@cdc.gov>
2025-01-04 17:31:13 -0500

    docs

---
+++ docs/trait.impl/core/clone/trait.Clone.js
@@ -0,0 +1,5 @@
+(function() {var implementors = {
+"fasten":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.77.0/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"struct\" href=\"fasten/io/seq/struct.Seq.html\" title=\"struct fasten::io::seq::Seq\">Seq</a>"]],
+"fasten_convert":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.77.0/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"struct\" href=\"fasten_convert/struct.FastenSeq.html\" title=\"struct fasten_convert::FastenSeq\">FastenSeq</a>"]],
+"fasten_sort":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.77.0/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"struct\" href=\"fasten_sort/struct.Seq.html\" title=\"struct fasten_sort::Seq\">Seq</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ docs/trait.impl/core/iter/traits/iterator/trait.Iterator.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"fasten":[["impl&lt;R: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.77.0/std/io/trait.Read.html\" title=\"trait std::io::Read\">Read</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.77.0/core/iter/traits/iterator/trait.Iterator.html\" title=\"trait core::iter::traits::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"fasten/io/fastq/struct.FastqReader.html\" title=\"struct fasten::io::fastq::FastqReader\">FastqReader</a>&lt;R&gt;"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
