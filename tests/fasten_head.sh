9ee3d6effc7f598cd29827f355b1873ea840535e
Lee Katz <lskatz@gmail.com>
2025-10-16 22:27:16 -0400

    md5sum comparison

--- tests/fasten_head.sh
+++ tests/fasten_head.sh
@@ -10,9 +10,10 @@ INPUT=testdata/four_reads.pe.fastq
 
 reads=$(for i in $(seq 1 1000); do cat $INPUT; done;)
 not_downsampled=$(echo "$reads" | ./target/debug/fasten_sample)
-first_ten_reads=$(echo "$reads" | head -n 40)
 
-fasten_head=$(echo "$reads" | ./target/debug/fasten_head --reads 10)
+# Use md5sum to compare
+first_ten_reads=$(echo "$reads" | head -n 40 | md5sum)
+fasten_head=$(echo "$reads" | ./target/debug/fasten_head --reads 10 | md5sum)
 
 if [ "$fasten_head" != "$first_ten_reads" ]; then
   echo "fasten_head does not match head -n 40"
