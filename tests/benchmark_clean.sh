cfa5208ad8abe9ac0de1007a658dc88479d8a5e5
Lee Katz <lskatz@gmail.com>
2025-10-20 11:50:34 -0400

    tests with png artifacts

--- tests/benchmark_clean.sh
+++ tests/benchmark_clean.sh
@@ -10,7 +10,7 @@ export PATH=$thisDir/../target/release:$PATH
 # get first 100 reads for any fastq file
 hyperfine --export-json=$reportsDir/clean.json --warmup 2 --shell $SHELL --runs $num_runs \
   -n "fasten_clean" "cat $large_R1 | fasten_clean --min-length 0 --min-trim-quality 5" \
-  -n "seqtk trimfq" "cat $large_R1 | seqtk trimfq -q 0.1"
+  -n "seqtk trimfq" "cat $large_R1 | seqtk trimfq -q 0.1 -"
 
 plot_whisker.py --title "Clean (reps=$num_runs)" --output $reportsDir/clean.json.png $reportsDir/clean.json
 
--- tests/benchmark_trim.sh
+++ tests/benchmark_trim.sh
@@ -10,7 +10,7 @@ export PATH=$thisDir/../target/release:$PATH
 # get first 100 reads for any fastq file
 hyperfine --export-json=$reportsDir/trim.json --warmup 2 --shell $SHELL --runs $num_runs \
   -n "fasten_trim" "cat $large_R1 | fasten_trim -f 5 -l 10" \
-  -n "seqtk trimfq" "cat $large_R1 | seqtk trimfq -b 5 -e 90" \
+  -n "seqtk trimfq" "cat $large_R1 | seqtk trimfq -b 5 -e 90 -" \
   -n "seqfu cat" "cat $large_R1 | seqfu cat --trim-front 5 --trim-tail 90"
 
 plot_whisker.py --title "Trim (reps=$num_runs)" --output $reportsDir/trim.json.png $reportsDir/trim.json
