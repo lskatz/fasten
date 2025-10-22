e5be70a0e31b4f242d42e7307321650c1df6962c
Lee Katz <lskatz@gmail.com>
2025-10-22 16:59:41 -0400

    make large test file larger

--- tests/lib/benchmark.sh
+++ tests/lib/benchmark.sh
@@ -55,7 +55,9 @@ if [[ ! -s $largest ]]; then
   # Create largest sorted file if not present
   for i in `seq 1 $multiplier`; do
     for j in `seq 1 $multiplier`; do
-      cat $large_sorted
+      for k in `seq 1 $multiplier`; do
+        cat $large_sorted
+      done
     done
   done > $largest
 fi
